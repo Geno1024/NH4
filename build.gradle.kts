8c6b1696b339728ba3577727f21c11c0b543597e
Y. Z. Chen <754097987@qq.com>
2025-08-22 00:55:32 +0800

    [Update] project: bump Gradle version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     kotlin("jvm") version "1.9.23" apply false
-    kotlin("android") version "2.0.0" apply false
+    kotlin("android") version "2.2.0" apply false
     id("com.android.application") version "8.11.0" apply false
 }
