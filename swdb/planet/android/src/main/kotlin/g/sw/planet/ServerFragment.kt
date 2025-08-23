72e302a9c65ae1d9728c37ab7a50f17bd3a58b7f
Y. Z. Chen <754097987@qq.com>
2025-08-23 13:51:40 +0800

    [Update] swdb/planet: save the server.

--- swdb/planet/android/src/main/kotlin/g/sw/planet/ServerFragment.kt
+++ swdb/planet/android/src/main/kotlin/g/sw/planet/ServerFragment.kt
@@ -1,15 +1,34 @@
 package g.sw.planet
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class ServerFragment : Fragment()
 {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
-    {
-        return inflater.inflate(R.layout.fragment_server, container, false)
-    }
+    val SP_SERVER = "server"
+
+    lateinit var sp: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        inflater.inflate(R.layout.fragment_server, container, false).apply {
+            sp = context?.getSharedPreferences("server", Context.MODE_PRIVATE)!!
+            editor = sp.edit()
+
+            val serverInput = findViewById<EditText>(R.id.server_input)
+            serverInput.setText(sp.getString(SP_SERVER, "") ?: "")
+
+            findViewById<Button>(R.id.server_input_confirm).setOnClickListener {
+                editor.putString(SP_SERVER, serverInput.text.toString())
+                editor.commit()
+                Toast.makeText(context, R.string.server_saved, Toast.LENGTH_SHORT).show()
+            }
+        }
 }
