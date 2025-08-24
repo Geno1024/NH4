ac34a69f7cf0b9ad59a805cfff5cc9e6d6c89926
Y. Z. Chen <754097987@qq.com>
2025-08-24 12:29:42 +0800

    [Update] swdb/star: add build count.

--- swdb/star/build.gradle.kts
+++ swdb/star/build.gradle.kts
@@ -1,3 +1,37 @@
+import g.buildsrc.BuildCount
+
 plugins {
     kotlin("jvm")
 }
+
+dependencies {
+    implementation(project(":swdb:protocol"))
+}
+
+// <editor-fold desc="Build Count">
+val run = BuildCount(project, "run")
+
+val runCount = tasks.register("runCount") {
+    group = "buildCount"
+    doLast {
+        run.inc()
+    }
+}
+
+val jarCount = BuildCount(project, "jar")
+
+val jar = tasks.register("jarCount") {
+    group = "buildCount"
+    doLast {
+        jarCount.inc()
+    }
+}
+
+tasks.withType<JavaCompile> {
+    dependsOn(run)
+}
+
+tasks.withType<Jar> {
+    dependsOn(jar)
+}
+// </editor-fold>
