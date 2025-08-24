f20dc758bc2729e74b7a94487f5bf8c9f2d40789
Y. Z. Chen <754097987@qq.com>
2025-08-24 11:13:28 +0800

    [Update] start writing star.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,3 +20,4 @@ dependencyResolutionManagement {
 }
 
 include(":swdb:planet:android")
+include(":swdb:star")
---
+++ swdb/star/src/main/kotlin/g/sw/Star.kt
@@ -0,0 +1,10 @@
+package g.sw
+
+object Star
+{
+    @JvmStatic
+    fun main(args: Array<String>)
+    {
+
+    }
+}
