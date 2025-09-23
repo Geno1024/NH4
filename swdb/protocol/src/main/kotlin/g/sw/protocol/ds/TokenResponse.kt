ecd839af67f3bd83190480a55d61f6a2805be1b3
Y. Z. Chen <754097987@qq.com>
2025-09-23 20:12:46 +0800

    [Update] swdb/protocol: allow wrong TokenResponse.

--- swdb/protocol/src/main/kotlin/g/sw/protocol/ds/TokenResponse.kt
+++ swdb/protocol/src/main/kotlin/g/sw/protocol/ds/TokenResponse.kt
@@ -1,20 +1,29 @@
 package g.sw.protocol.ds
 
+import g.sw.protocol.box.BBinary
 import g.sw.protocol.box.BString
 import java.security.SecureRandom
 
 data class TokenResponse(
+    val verified: BBinary,
     val username: BString,
     val token: BString
 ) : IDS
 {
     companion object
     {
-        fun generate(request: TokenRequest, challenge: TokenChallenge): TokenResponse = TokenResponse(
+        fun generate(verified: Boolean, request: TokenRequest, challenge: TokenChallenge): TokenResponse = TokenResponse(
+            verified = BBinary(verified),
             username = request.username,
-            token = with (ByteArray(32) { 0 }) {
-                SecureRandom().nextBytes(this)
-                BString(challenge.timestamp.str + "," + toHexString())
+            token = if (verified)
+            {
+                with(ByteArray(32)) {
+                    SecureRandom().nextBytes(this)
+                    BString(challenge.timestamp.str + "," + toHexString())
+                }
+            } else
+            {
+                BString()
             }
         )
     }
